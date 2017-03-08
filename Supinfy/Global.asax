bd7a2d0f858737818dd5978a5f5eccbaa750ce80
ClemH <clement.hageaux@gmail.com>
2017-03-08 10:25:56 +0100

    1er commit (architecture clean)

---
+++ Supinfy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Supinfy.MvcApplication" Language="C#" %>
---
+++ Supinfy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Supinfy.Startup))]
+namespace Supinfy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
